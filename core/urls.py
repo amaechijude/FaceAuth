b7061e4bebdc2def453aa1658f89f598838292c4
amaechijude <ugwuamaechi5@gmail.com>
2025-01-07 09:46:30 +0100

    camera capture done

--- core/urls.py
+++ core/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('user/register', views.register_user, name='register_user'),
-    path('user/login', views.login_user, name='login_user'),
+    path('register', views.register_user, name='register_user'),
+    path('login', views.login_user, name='login_user'),
     path('logout', views.logout_user, name='logout_user'),
     path('', views.index_page, name='index_page')
 ]
